9770887c5dc3af7f6469f91d3902e4711994d38f
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-04-18 20:46:25 +0300

    13.3-5 refactored the app

---
+++ controllers/blogs.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const { Blog } = require('../models');
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
+router.get('/', async (req, res) => {
+  const notes = await Blog.findAll();
+  res.json(notes);
+});
+
+router.post('/', blogFinder, async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    blog.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
