5964f05039832319ca41ef6deb8257abd0b0bd9e
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-04-19 20:02:44 +0300

    refactor

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const { connectToDatabase } = require('./utils/db');
 const app = express();
 
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
