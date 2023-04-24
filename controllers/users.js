7f8cdbda4f1f02abd26a8993754ef5311d57e787
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-04-24 16:03:54 +0300

    refactor and bug fix

--- controllers/users.js
+++ controllers/users.js
@@ -9,10 +9,12 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const user = await User.create(req.body);
+    const user = { ...req.body, created_at: new Date() };
+
+    await User.create(req.body);
     res.json(user);
   } catch (error) {
-    return res.status(400).json({ error });
+    return res.status(400).json({ error: error.message });
   }
 });
 
@@ -20,6 +22,17 @@ router.get('/:id', async (req, res) => {
   const user = await User.findByPk(req.params.id);
   if (user) {
     res.json(user);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put('/:username', async (req, res) => {
+  if (!req.body.username) res.status(400).end();
+  if (req.blog) {
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json(req.blog);
   } else {
     res.status(404).end();
   }
