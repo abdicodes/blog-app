221932f4c6ddf971eb62cf8e7d2d72e694a8fecd
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-04-24 22:38:31 +0300

    13.8 implemented all routing

--- controllers/users.js
+++ controllers/users.js
@@ -9,12 +9,10 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const user = { ...req.body, created_at: new Date() };
-
-    await User.create(req.body);
+    const user = await User.create(req.body);
     res.json(user);
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ error });
   }
 });
 
@@ -29,10 +27,17 @@ router.get('/:id', async (req, res) => {
 
 router.put('/:username', async (req, res) => {
   if (!req.body.username) res.status(400).end();
-  if (req.blog) {
-    req.blog.likes = req.body.likes;
-    await req.blog.save();
-    res.json(req.blog);
+
+  const user = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+  });
+
+  if (user) {
+    user.username = req.body.username;
+    await user.save();
+    res.json(user);
   } else {
     res.status(404).end();
   }
