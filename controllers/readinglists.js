2218c38b48977091b556714fdfb5fe74a549f818
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-05-02 04:33:16 +0300

    13.22 reading list PUT req with authen.

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const middleware = require('../utils/middleware');
-const { ReadingList } = require('../models');
+const { ReadingList, User, Blog } = require('../models');
 const { userExtractor } = require('../utils/middleware');
 const { Op } = require('sequelize');
 const sequelize = require('sequelize');
@@ -10,4 +10,17 @@ router.post('/', async (req, res) => {
   res.json(readingList);
 });
 
+router.put('/:id', userExtractor, async (req, res) => {
+  const reading = await ReadingList.findByPk(req.params.id);
+  if (!reading) {
+    res.status(404).end();
+  } else if (!(reading.userId === req.user.id)) {
+    res.status(400).end();
+  } else {
+    console.log(reading.userId, req.user.id);
+    reading.read = req.body.read;
+    await reading.save();
+    res.json(reading);
+  }
+});
 module.exports = router;
